d117afc8a383983f83f9502cb586c87ca53bf5d5
Alistair Spragg <alistair.spragg@gmail.com>
2021-07-01 15:39:48 +0100

    Add XML comments

--- StringCommandParser/Attributes/VerbAttribute.cs
+++ StringCommandParser/Attributes/VerbAttribute.cs
@@ -1,11 +1,31 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace StringCommandParser.Attributes
 {
-    class VerbAttribute : Attribute
+    /// <summary>
+    /// Attribute for usage on classes to denote a verb
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, Inherited = true)]
+    public class VerbAttribute : Attribute
     {
+        /// <summary>
+        /// The name of the class as it should be referenced in the command.
+        /// </summary>
+        [Required()]
         public string Verb { get; set; }
+
+        /// <summary>
+        ///  Text to return when the parsing fails.
+        /// </summary>
+        [Required()]
         public string HelpText { get; set; }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="VerbAttribute"/> class.
+        /// </summary>
+        /// <param name="verb"></param>
+        /// <param name="helpText"></param>
         public VerbAttribute(string verb, string helpText) => (Verb, HelpText) = (verb, helpText);
     }
 }

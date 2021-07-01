9f7b70e532ebc0bbcc797ca7aca2ff16f0d377cc
Alistair Spragg <alistair.spragg@gmail.com>
2021-07-01 15:57:20 +0100

    Add XML comments to parser

--- StringCommandParser/Parser.cs
+++ StringCommandParser/Parser.cs
@@ -5,12 +5,26 @@ using System.Reflection;
 
 namespace StringCommandParser
 {
-    class Parser : IParser
+    /// <summary>
+    /// Contains argument parsing method and necessary data
+    /// </summary>
+    public class Parser : IParser
     {
-        public Type[] Types { get; set; }
+        /// <summary>
+        /// Declared types for classes that contain commands
+        /// </summary>
+        internal Type[] Types { get; set; }
 
-        public ParserResult Result { get; set; }
+        /// <summary>
+        /// The response of the parser
+        /// </summary>
+        internal ParserResult Result { get; set; }
 
+        /// <summary>
+        /// Parses a command in string form by checking against known verbs, which are classes decorated using <see cref="VerbAttribute"/>.
+        /// </summary>
+        /// <param name="args">A <see cref="string"/> array of command line arguments.</param>
+        /// <param name="types">The declaration types that have <see cref="VerbAttribute"/> decorations.</param>
         public IParser ParseArgs(string[] args, params Type[] types)
         {
             Types = types;
@@ -67,10 +81,52 @@ namespace StringCommandParser
         }
 
         #region extra_parser_types
+        /// <summary>
+        /// Parses a command in string form by checking against known verbs, which are classes decorated using <see cref="VerbAttribute"/>.
+        /// </summary>
+        /// <typeparam name="T1"><see cref="VerbAttribute"/> decorated class.</typeparam>
+        /// <param name="args">A <see cref="string"/> array containing the command arguments.</param>
+        /// <returns></returns>
         public IParser ParseArgs<T1>(string[] args) => ParseArgs(args, new[] { typeof(T1) });
+
+        /// <summary>
+        /// Parses a command in string form by checking against known verbs, which are classes decorated using <see cref="VerbAttribute"/>.
+        /// </summary>
+        /// <typeparam name="T1"><see cref="VerbAttribute"/> decorated class.</typeparam>
+        /// <typeparam name="T2"><see cref="VerbAttribute"/> decorated class.</typeparam>
+        /// <param name="args">A <see cref="string"/> array containing the command arguments.</param>
+        /// <returns></returns>
         public IParser ParseArgs<T1, T2>(string[] args) => ParseArgs(args, new[] { typeof(T1), typeof(T2) });
+
+        /// <summary>
+        /// Parses a command in string form by checking against known verbs, which are classes decorated using <see cref="VerbAttribute"/>.
+        /// </summary>
+        /// <typeparam name="T1"><see cref="VerbAttribute"/> decorated class.</typeparam>
+        /// <typeparam name="T2"><see cref="VerbAttribute"/> decorated class.</typeparam>
+        /// <typeparam name="T3"><see cref="VerbAttribute"/> decorated class.</typeparam>
+        /// <param name="args">A <see cref="string"/> array containing the command arguments.</param>
+        /// <returns></returns>
         public IParser ParseArgs<T1, T2, T3>(string[] args) => ParseArgs(args, new[] { typeof(T1), typeof(T2), typeof(T3) });
+
+        /// <summary>
+        /// Parses a command in string form by checking against known verbs, which are classes decorated using <see cref="VerbAttribute"/>.
+        /// </summary>
+        /// <typeparam name="T1"><see cref="VerbAttribute"/> decorated class.</typeparam>
+        /// <typeparam name="T2"><see cref="VerbAttribute"/> decorated class.</typeparam>
+        /// <typeparam name="T3"><see cref="VerbAttribute"/> decorated class.</typeparam>
+        /// <typeparam name="T4"><see cref="VerbAttribute"/> decorated class.</typeparam>
+        /// <param name="args">A <see cref="string"/> array containing the command arguments.</param>
         public IParser ParseArgs<T1, T2, T3, T4>(string[] args) => ParseArgs(args, new[] { typeof(T1), typeof(T2), typeof(T3), typeof(T4) });
+
+        /// <summary>
+        /// Parses a command in string form by checking against known verbs, which are classes decorated using <see cref="VerbAttribute"/>.
+        /// </summary>
+        /// <typeparam name="T1"><see cref="VerbAttribute"/> decorated class.</typeparam>
+        /// <typeparam name="T2"><see cref="VerbAttribute"/> decorated class.</typeparam>
+        /// <typeparam name="T3"><see cref="VerbAttribute"/> decorated class.</typeparam>
+        /// <typeparam name="T4"><see cref="VerbAttribute"/> decorated class.</typeparam>
+        /// <typeparam name="T5"><see cref="VerbAttribute"/> decorated class.</typeparam>
+        /// <param name="args">A <see cref="string"/> array containing the command arguments.</param>
         public IParser ParseArgs<T1, T2, T3, T4, T5>(string[] args) => ParseArgs(args, new[] { typeof(T1), typeof(T2), typeof(T3), typeof(T4), typeof(T5) });
         #endregion
     }

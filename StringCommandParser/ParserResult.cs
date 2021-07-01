b00ab15d59c21526f0313be602fb8ed882e7527b
Alistair Spragg <alistair.spragg@gmail.com>
2021-07-01 15:46:53 +0100

    Add class to contain output

---
+++ StringCommandParser/ParserResult.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+
+namespace StringCommandParser
+{
+    /// <summary>
+    /// Defines the output class of the <see cref="IParser"/> class.
+    /// </summary>
+    public class ParserResult
+    {
+        /// <summary>
+        /// Contains the method being invoked by the command
+        /// </summary>
+        public MethodInfo Method { get; set; }
+
+        /// <summary>
+        /// Contains the verb class type being invoked by the command
+        /// </summary>
+        public Type Verb { get; set; }
+
+        /// <summary>
+        /// Contains the args array initially supplied to the parser
+        /// </summary>
+        public string[] Args { get; set; }
+
+        /// <summary>
+        /// True if the command was parsed successfully
+        /// </summary>
+        public bool Parsed { get; set; }
+    }
+}
